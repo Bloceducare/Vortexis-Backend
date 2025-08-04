5b3d26a3ef7e9eb514e09d349f457f3e03edb64e
Armolas <armolas06@gmail.com>
2025-08-04 17:49:59 +0100

    feat: implement password reset email functionality with token expiration notice

--- accounts/utils.py
+++ accounts/utils.py
@@ -24,3 +24,28 @@ def send_otp_mail(email):
     message = f'Hi {user.first_name},\n\nThank you for signing up on Vortexis. Please use the following OTP to verify your account.\n\nOTP: {otp}\n\nIf you did not sign up on Vortexis, please ignore this email.\n\nRegards,\nVortexis Team' 
     email = EmailMessage(subject, message, from_email=from_email, to=[email])
     email.send(fail_silently=True)
+
+
+def send_password_reset_email(user, reset_token):
+    from_email = settings.DEFAULT_EMAIL_HOST
+    
+    subject = 'Password Reset - Vortexis'
+    message = f'''Hi {user.first_name},
+
+You have requested to reset your password for your Vortexis account.
+
+Please use the following token to reset your password:
+
+Token: {reset_token.token}
+
+This token will expire in 1 hour.
+
+If you did not request a password reset, please ignore this email and your password will remain unchanged.
+
+For security reasons, this reset link can only be used once.
+
+Regards,
+Vortexis Team'''
+    
+    email = EmailMessage(subject, message, from_email=from_email, to=[user.email])
+    email.send(fail_silently=True)
